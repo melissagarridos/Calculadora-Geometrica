180c5fad6e6b82f7d3e2707ff23ebaa1fd15e79d
Mel <melissagarridosierra@gmail.com>
2026-03-14 08:07:44 -0500

    Cambios

--- calculadora_geometrica.py
+++ calculadora_geometrica.py
@@ -1,22 +1,26 @@
 # Melissa Garrido
-
 pi = 3.14
 
+print()
 print("Welcome!")
 print("This is a geometric calculator")
+print()
 
-calculator = 0
+calculator = input("Do you want to perform an operation? (yes / no): ").lower().strip()
 
-while calculator == 0:
+while calculator == "yes":
 
-    print("""Posible options
+    print()
+    print("""Please select one option:
           - 2D
           - 3D
           - Exit""")
+    print()
 
     choice = input("Write your choice: ").lower().strip()
 
-    if choice == "2d":
+
+    while choice == "2d":
 
         print("""You have the following options:
             1. Square
@@ -28,167 +32,119 @@ while calculator == 0:
         
         shape = input("Choose a number: ").strip()
 
-        if shape.isdigit():
-
+        try:
             shape = int(shape)
 
             if shape == 1:
 
                 print("Square")
 
-                l = input("Please insert length: ").strip()
-
-                if l.isdigit():
-
-                    l = float(l)
-
+                try:
+                    l = float(input("Please insert length: ").strip())
                     area = l ** 2
-
                     perimeter = 4 * l
-
                     print("Area: ", area)
                     print("Perimeter: ", perimeter)
+                    break
 
-                else:
-                    print("Insert a valid digit")
+                except ValueError:
+                    print("Insert a valid number")
                     continue
 
             elif shape == 2:
 
                 print("Rectangle")
 
-                l = input("Please insert length: ").strip()
-
-                w = input("Please insert width: ").strip()
-
-                if l.isdigit() and w.isdigit():
-
-                    l = float(l)
-
-                    w = float(w)
-
-                    area = 2 * l + 2 * w
-
-                    perimeter = l * w
-
+                try:
+                    l = float(input("Please insert length: ").strip())
+                    w = float(input("Please insert width: ").strip())
+                    perimeter = 2 * l + 2 * w
+                    area = l * w
                     print("Area: ", area)
                     print("Perimeter: ", perimeter)
+                    break
 
-                else:
-                    print("Insert a valid digit")
+                except ValueError:
+                    print("Insert a valid number")
                     continue
 
             elif shape == 3:
 
                 print("Triangle")
 
-                a1 = input("Please insert angle 1: ").strip()
-
-                a2 = input("Please insert angle 2: ").strip()
-
-                b = input("Please insert base: ").strip()
-
-                h = input("Please insert height: ").strip()
-
-                if a1.isdigit() and a2.isdigit() and b.isdigit() and h.isdigit():
-
-                    a1 = float(a1)
-
-                    a2 = float(a2)
-
-                    b = float(b)
-
-                    h = float(h)
-
+                try:
+                    a1 = float(input("Please insert angle 1: ").strip())
+                    a2 = float(input("Please insert angle 2: ").strip())
+                    b = float(input("Please insert base: ").strip())
+                    h = float(input("Please insert height: ").strip())
                     area = (b * h) / 2
-
                     a3 = 180 - a1 - a2
-
-                    perimeter = a1 + a2 + a3
-
+                    hypotenuse = (b ** 2 + h ** 2) ** 0.5
+                    perimeter = b + h + hypotenuse
                     print("Area: ", area)
                     print("Perimeter: ", perimeter)
                     print("Angle 3: ", a3)
+                    break
 
-                else:
-                    print("Insert a valid digit")
+                except ValueError:
+                    print("Insert a valid number")
                     continue
 
             elif shape == 4:
 
                 print("Circle")
 
-
-                r = input("Please insert radius: ").strip()
-
-
-                if r.isdigit():
-
-                    r = float(r)
-
+                try:
+                    r = float(input("Please insert radius: ").strip())
                     d = r * 2
-
                     area = pi * r ** 2
-
                     perimeter = 2 * pi * r
-
                     print("Area: ", area)
                     print("Perimeter: ", perimeter)
                     print("Diameter: ", d)
+                    break
 
-                else:
-                    print("Insert a valid digit")
+                except ValueError:
+                    print("Insert a valid number")
                     continue
 
             elif shape == 5:
 
                 print("Triangle Rectangle")
 
-                a1 = input("Please insert one angle: ").strip()
-
-                a2 = 90
-
-                b = input("Please insert base: ").strip()
-
-                h = input("Please insert height: ").strip()
-
-                if a1.isdigit() and b.isdigit() and h.isdigit():
-
-                    a1 = float(a1)
-
-                    a2 = float(a2)
-
-                    b = float(b)
-
-                    h = float(h)
-
+                try:
+                    a1 = float(input("Please insert one angle: ").strip())
+                    a2 = 90.0
+                    b = float(input("Please insert base: ").strip())
+                    h = float(input("Please insert height: ").strip())
                     area = (b * h) / 2
-
                     a3 = 180 - a1 - a2
-
-                    perimeter = a1 + a2 + a3
-
                     hypotenuse = (b ** 2 + h ** 2) ** 0.5
-
+                    perimeter = b + h + hypotenuse
                     print("Area: ", area)
                     print("Perimeter: ", perimeter)
-                    print("Angle 1", a1)
-                    print("Angle 2", a2)
+                    print("Angle 1:", a1)
+                    print("Angle 2:", a2)
                     print("Angle 3: ", a3)
                     print("Hypotenuse: ", hypotenuse)
-                
-                else:
-                    print("Insert a valid digit")
+                    break
+
+                except ValueError:
+                    print("Insert a valid number")
                     continue
 
             elif shape == 6:
+                break
+
+            else:
+                print("Choose a valid option")
                 continue
 
-        else:
+        except ValueError:
             print("Choose a valid option")
             continue
-
-    elif choice == "3d":
+    
+    while choice == "3d":
 
         print("3D")
 
@@ -201,120 +157,95 @@ while calculator == 0:
         
         shape = input("Choose a number: ").strip()
 
-        if shape.isdigit():
-
+        try:
             shape = int(shape)
 
             if shape == 1:
 
                 print("Cube")
 
-                l = input("Please insert length: ").strip()
-
-                if l.isdigit():
-
-                    l = float(l)
-
+                try:
+                    l = float(input("Please insert length: ").strip())
                     surface_area = 6 * l ** 2
-
-                    volumen = l ** 3
-
+                    volume = l ** 3
                     print("Surface Area: ", surface_area)
-                    print("Volumen: ", volumen)
+                    print("Volume: ", volume)
+                    break
 
-                else:
-                    print("Insert a valid digit")
+                except ValueError:
+                    print("Insert a valid number")
                     continue
 
             elif shape == 2:
 
                 print("Sphere")
 
-                r = input("Please insert radius: ").strip()
-
-                if r.isdigit():
-
-                    r = float(r)
-
+                try:
+                    r = float(input("Please insert radius: ").strip())
                     area = 4 * pi * r ** 2
-
                     volume = (4/3) * pi * r ** 3
-
                     print("Surface Area: ", area)
                     print("Volume: ", volume)
+                    break
 
-                else:
-                    print("Insert a valid digit")
+                except ValueError:
+                    print("Insert a valid number")
                     continue
 
             elif shape == 3:
                 
                 print("Cylinder")
 
-                r = input("Please insert radius: ").strip()
-
-                h = input("Please insert height: ").strip()
-
-                if r.isdigit() and h.isdigit():
-
-                    r = float(r)
-
-                    h = float(h)
-
+                try:
+                    r = float(input("Please insert radius: ").strip())
+                    h = float(input("Please insert height: ").strip())
                     area = 2 * pi * r * (r + h)
-
                     volume = pi * r ** 2 * h
-
                     print("Surface Area: ", area)
                     print("Volume: ", volume)
+                    break
 
-                else:
-                    print("Insert a valid digit")
+                except ValueError:
+                    print("Insert a valid number")
                     continue
-
 
             elif shape == 4:
                 
                 print("Rectangular Prism")
 
-                l = input("Please insert length: ").strip()
-
-                h = input("Please insert height: ").strip()
-
-                w = input("Please insert width: ").strip()
-
-                if l.isdigit() and h.isdigit() and h.isdigit():
-
-                    l = float(l)
-
-                    h = float(h)
-
-                    w = float(w)
-
+                try:
+                    l = float(input("Please insert length: ").strip())
+                    h = float(input("Please insert height: ").strip())
+                    w = float(input("Please insert width: ").strip())
                     area = 2 * (l*w + l*h + w*h)
-
                     volume = l * w * h
-
                     print("Surface Area: ", area)
                     print("Volume: ", volume)
+                    break
 
-                else:
-                    print("Insert a valid digit")
+                except ValueError:
+                    print("Insert a valid number")
                     continue
 
             elif shape == 5:
-                continue
+                break
 
             else:
                 print("Choose a valid option")
                 continue
-    
-    elif choice == "exit":
+
+        except ValueError:
+            print("Choose a valid option")
+            continue
+
+    while choice == "exit":
 
         print("See you soon!")
+        calculator = "no"
         break
 
-
     else: 
-        print("Choose a valid option")
         continue
+
+if calculator == "no":
+    print("See you soon!")
